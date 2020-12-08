24037d64006db9f794dae04aa212300787d2cc3c
aslawny <alexis.slawny@gmail.com>
2020-12-08 22:03:45 +0100

    router ok

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/user">User</router-link> |
+      <router-link to="/tableau">Tableau</router-link>
+    </div>
+    <router-view />
+  </div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
---
+++ src/views/Tableau.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Tableau Page</h1>
+</template>
