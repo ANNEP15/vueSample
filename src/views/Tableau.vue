0d6c2f9821bea9a3874a98fbb338d2db3f4c7caf
Annette Plaisimont <plaisimont320@hotmail.com>
2021-02-26 18:28:40 +0100

    tableaux new

--- src/views/Tableau.vue
+++ src/views/Tableau.vue
@@ -14,7 +14,7 @@
       class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden"
     >
       <div class="my-3 py-3">
-        <h2 class="display-5">Another headline<img class="w-15" src="../assets/FACE.jpg"/></h2>
+     
         <p class="lead">And an even wittier subheading.</p>
       </div>
       <div
