ea79dab0786e35b385acf06b20875a5c73b08c42
Annette Plaisimont <plaisimont320@hotmail.com>
2021-03-02 18:25:03 +0100

    fix version app

--- src/App.vue
+++ src/App.vue
@@ -38,82 +38,6 @@
         </div>
       </nav>
     </div>
-    <template>
-  <div class="container-fluid">
-    <div class="text-center">
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li
-            data-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            class="active"
-          ></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                  </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="w-15" src="../assets/FACE.jpg"/>
-          </div>
-          <div class="carousel-item">
-            <img class="w-30" src="../assets/paysage.png"/>
-          </div>
-          <div class="carousel-item">
-            <img class="w-30" src="../assets/venise.png"/>
-          </div>
-          <div class="carousel-item">
-            <img class="w-20" src="../assets/COLOR2.jpg" />
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
-
-<style>
-.carousel-control-prev-icon {
-  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='red' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
-}
-
-.carousel-control-next-icon {
-  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='red' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E");
-}
-.carousel-inner {
-  height: 500px;
-}
-
-.carousel-inner > .active {
-  position: relative;
-  top: 50%;
-  transform: translateY(-50%);
-}
-.carousel-inner > .next,
-.carousel-inner > .prev {
-  top: 50%;
-  transform: translateY(-50%);
-}
-</style>
-
     <!-- current page -->
     <router-view />
     <!-- footer-->
@@ -123,15 +47,18 @@
           <HelloWorld msg="Bienvenue sur le site d'Annette" />
         </li>
       </ul>
-      <a class="navbar-brand" href="mailto:'plaisimont320@hotmail.com'"> Nous Contacter</a>
-    </nav>  
-     </div>  
-     </template>
-  
-    <script>
+      <a class="navbar-brand" href="mailto:'john.smith@toto.com'"> Nous Contacter</a>
+    </nav>
+  </div>
+</template>
+
+<script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
- name: "App",  components: {HelloWorld
-  };
-  </script>
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
